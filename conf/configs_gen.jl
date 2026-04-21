a6093912a1104e7dc0cc5394c8423a3bdd1ccba2
artem-gordeev <ar1emg0rdeev@gmail.com>
2026-04-21 17:05:43 +0300

    not working config

--- conf/configs_gen.jl
+++ conf/configs_gen.jl
@@ -1,17 +1,16 @@
 using NeutralAtoms
 using QuantumOptics
 using Serialization
-using DataFrames
-using CSV
-using DelimitedFiles 
+
 
 function get_6P_config()
-       m = 86.9091835;            # Atom params
-       T = 1.0 # 70.0;
+       # Atom params
+       m = 86.9091835;     
+       T = 70.0;
        atom_params = [m, T];
-
-       U0 = 800 * 0.65; #797.7 changed 26.02.2026
-       w0 = 1.42; # Trap params
+       # Trap params
+       U0 = 800.0*0.65; #max * эффективность использования
+       w0 = 1.42; 
        λ0 = 0.813;
        M2 = 1.0;
        z0 = w0_to_z0(w0, λ0, M2);
@@ -30,44 +29,46 @@ function get_6P_config()
        first_laser_phase_amplitudes = ϕ_amplitudes(f, first_laser_phase_params);
        second_laser_phase_amplitudes = ϕ_amplitudes(f, second_laser_phase_params);
 
-       λ2 = 1.012;        ### Excitation beam parameters
+       ### Excitation beam parameters
        λ1 = 0.42;
+       λ2 = 1.012;
+       w1 = 8.0;
        w2 = 20.0;
-       w1 = 8.0 #10.0;
-       z2 = w0_to_z0(w2, λ2);
        z1 = w0_to_z0(w1, λ1);
+       z2 = w0_to_z0(w2, λ2);
 
-       Δ0 = 2.0*π * 2000 #870 #1600.0 #1000.0 #* 1600
-       Ω = 2π * 2.3 # 2.0;
-       a = 1 
-       Ω1 = 1/a * sqrt(2* Δ0 * Ω);
-       Ω2 = a * sqrt(2* Δ0 * Ω);
-       θ2 = π;
+       Δ0 = 2.0*π * 800 #2000.0;
+       Ω = 2π * 2.3;
+       Ω1 = sqrt(2* Δ0 * Ω);
+       Ω2 = sqrt(2* Δ0 * Ω);
+       # Orientation and flat-top param, n=1 - gauss
+       # θr = π/2;
        θ1 = 0.0;
-       n2 = 1;
+       θ2 = π;
        n1 = 1;
+       n2 = 1;
 
-       first_laser_params = Dict("Ω" => Ω2,"w0" => w2,"z0" => z2,
-              "θ" => θ2,"n_sg" => n2,"type" => "gauss") #first_laser_params = [Ωr, wr, zr, θr] #, nr];
-       second_laser_params = Dict("Ω" => Ω1,"w0" => w1,"z0" => z1,
-              "θ" => θ1,"n_sg" => n1,"type" => "gauss")      #second_laser_params = [Ωb, wb, zb, θb] #, nb];
+       first_laser_params = Dict("Ω" => Ω1,"w0" => w1,"z0" => z1,
+              "θ" => θ1,"n_sg" => n1,"type" => "gauss") #first_laser_params = [Ωr, wr, zr, θr] #, nr];
+       second_laser_params = Dict("Ω" => Ω2,"w0" => w2,"z0" => z2,
+              "θ" => θ2,"n_sg" => n2,"type" => "gauss")      #second_laser_params = [Ωb, wb, zb, θb] #, nb];
 
-       detuning_params = [Δ0, -δ_twophoton(Ω2, Ω1, Δ0)]; #???
-
-       Γ =  8.475 #2.0*π * 5.75;
-       Γ0, Γ1, Γl = Γ/4, Γ/4, 2*Γ/4;
-       τr = 445.74; 
+       detuning_params = [Δ0, -δ_twophoton(Ω1, Ω2, Δ0)];
+       Γ = 8.475;
+       Γ0, Γ1, Γl = Γ/4, Γ/4, 2*Γ/4; #уточнить
+       # Quasiclassical calculations of BBR-induced depopulation rates and effective lifetimes
+       # of Rydberg nS, nP, and nD alkali-metal atoms with n ≤ 80. T = 300, n=60, S_1/2, Rb87
+       τr = 445.0; #from arc getStateLifetime: n3=74, l3=0, j3=0.5 
        Γr = 1/τr;
        decay_params = [Γ0, Γ1, Γl, Γr];
-       
-       #T0 = T_twophoton(Ω2, Ω1, Δ0) # Simulation params
-       #tspan = [0.0:T0/20:3.0;]; #15.0
+
+       # Simulation params
        T0 = 2.5 #T_twophoton(Ω1, Ω2, Δ0)
        tspan = [0.0:T0/200:T0;];
+       #tspan = [0.0:T0/20:5.5*T0;];
        ψ0 = ket_1;
        n_samples = 20;
        shift = [0.0,0.0,0.0]
-
        error_options = Dict("laser_noise" => false,
                         "spontaneous_decay_intermediate" => true,
                         "spontaneous_decay_rydberg" => true,
@@ -98,32 +99,27 @@ function get_6P_config()
               decay_params,
 
               error_options
-              );
- 
-       d = 2.7 #2.0;
-       #atom_centers = [[-d/2, 0.0, 0.0], [d/2, 0.0, 0.0]]
-       atom_centers = [[0.0, 0.0,-d/2], [0.0, 0.0,d/2]]
-       c6 = 2π * 135298
-       
-       ΔtoΩ = 0.377371 #идеальные
+       );
+
+       d = 2.7;
+       atom_centers = [[0.0, 0.0, -0.5*d], [0.0, 0.0, 0.5*d]]
+       c6 = 2π * 135298 #узнать для n=74
+       ΔtoΩ = 0.377371
        Ωτ = 4.29268
        ξ = 3.90242
-       
        ket_pos = (ket_0 + ket_1)/sqrt(2)
-       ψ0_cz = ket_pos ⊗ ket_pos
-       
+       ψ0_cz = ket_pos ⊗ ket_pos 
+
        Ω_twophoton = (2π/T_twophoton(Ω1, Ω2, Δ0))
        τ = 2π / (Ω_twophoton * sqrt(ΔtoΩ^2 + 2.0))
-
-       #Ω_twophoton = (2π/T0)
-       #τ = Ωτ/Ω_twophoton #τ = 2π / (Ω_twophoton * sqrt(ΔtoΩ^2 + 2.0))
        VV = c6 / d^6 #3.4^6 #       Ω_twophoton / 2 / V
-       detuning_params = [Δ0, ΔtoΩ * Ω_twophoton - δ_twophoton(Ω1, Ω2, Δ0)]; #ΔtoΩ = ΔtoΩ + Ω_twophoton/(2*VV)
+       #ΔtoΩ = ΔtoΩ + Ω_twophoton/(2*VV)
+       detuning_params = [Δ0, ΔtoΩ * Ω_twophoton - δ_twophoton(Ω1, Ω2, Δ0)];
        
        ϕ2 = 2*τ * ΔtoΩ * Ω_twophoton;
        ϕ1 = (ϕ2 - π)/2  
-       tspan_cz = [0.0, 2*τ];        #detuning_params = [Δ0, ΔtoΩ*Ω_twophoton + δ_twophoton(Ωr, Ωb, Δ0)];
-       
+       tspan_cz = [0.0, 2*τ];
+
        cfg_czlp = NeutralAtoms.CZLPConfig(
               tspan_cz,
               ψ0_cz,
@@ -151,28 +147,6 @@ function get_6P_config()
               ξ,
               ϕ1
        )
+
        return cfg, cfg_czlp
 end
-
-function get_5P_config()
-       _, cfg_CZ_5P = get_default_config()
-       ket_pos = (ket_0 + ket_1) / sqrt(2)
-       cfg_CZ_5P.ψ0 = ket_pos ⊗ ket_pos 
-
-       w = 2. ;
-       bl_lsr_prms = cfg_CZ_5P.second_laser_params
-       Ω0, w0, z0 = bl_lsr_prms["Ω"], bl_lsr_prms["w0"], bl_lsr_prms["z0"];
-       z = z0*(w/w0)^2        #cfg_CZ_6P.second_laser_params["type"] = "gauss" 
-       cfg_CZ_5P.second_laser_params["w0"] = w ; 
-       cfg_CZ_5P.second_laser_params["z"] = z;
-
-       Γ = 2.0*π * 5.75;
-       Γ0, Γ1, Γl = Γ/4, Γ/4, 2*Γ/4;
-       # Quasiclassical calculations of BBR-induced depopulation rates and effective lifetimes
-       # of Rydberg nS, nP, and nD alkali-metal atoms with n ≤ 80. T = 300, n=60, S_1/2, Rb87
-       τr = 111.74; 
-       Γr = 1/τr;
-       cfg_CZ_5P.decay_params = [Γ0, Γ1, Γl, Γr];
-
-       return cfg_CZ_5P
-end;
